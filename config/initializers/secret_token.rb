a8c9f1ad73bfd5f8bc1b33639a6582b0ad46e547
robin <robin.edman@gmail.com>
2014-01-21 11:20:37 +0000

    Empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Farsee::Application.config.secret_key_base = '63f34fcfd8e595e647805002ed67e17e96d02c94eeb27d67ed272c04ec8fefc8f3e6a54ccf906fdf39e8f3a252a45b9a7859402b549dbe9f881fe8d5defc8c0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Farsee::Application.config.session_store :cookie_store, key: '_farsee_session'
